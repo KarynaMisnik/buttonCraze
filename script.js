270b6a8f3d2a5c03465ac4ea886f75ed9bd7d918
KarynaMisnik <karinamisnik94@gmail.com>
2024-03-27 21:58:50 +0200

    first commit
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ script.js
@@ -0,0 +1,51 @@
+function copyCss(className) {
+  const elements = document.querySelectorAll(className);
+  const previousHTML = elements[0].innerHTML; // Assuming all elements with the class have the same HTML content
+  elements.forEach((element) => {
+    element.classList.add("copied");
+  });
+
+  // Get all style sheets
+  const styleSheets = document.styleSheets;
+
+  // Initialize a string to store CSS rules
+  let cssText = "";
+
+  // Loop through each style sheet
+  for (let i = 0; i < styleSheets.length; i++) {
+    const rules = styleSheets[i].cssRules;
+    if (rules) {
+      // Loop through each CSS rule
+      for (let j = 0; j < rules.length; j++) {
+        // Check if the rule is for the specified class
+        if (
+          rules[j].selectorText &&
+          rules[j].selectorText.includes(className)
+        ) {
+          cssText += rules[j].cssText + "\n";
+        }
+      }
+    }
+  }
+
+  copyToClipboard(cssText);
+  elements.forEach((element) => {
+    element.innerHTML = "Copied!";
+  });
+
+  setTimeout(() => {
+    elements.forEach((element) => {
+      element.innerHTML = previousHTML;
+      element.classList.remove("copied");
+    });
+  }, 1000);
+}
+
+function copyToClipboard(text) {
+  const el = document.createElement("textarea");
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+}
